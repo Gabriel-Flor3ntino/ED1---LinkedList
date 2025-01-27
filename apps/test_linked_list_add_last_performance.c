f7f54e9fc090db9fc2be19e0d42be1c55cf2c288
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2025-01-27 19:34:05 -0300

    Listas encadeadas simples - Estudos de ED1

---
+++ apps/test_linked_list_add_last_performance.c
@@ -0,0 +1,28 @@
+#include "linked_list.h"
+#include "mytime.h"
+
+
+int main(){
+  LinkedList *L_slow = LinkedList_create();
+  timer t1, t2;
+
+  t1 = tic();
+  for (int i = 0; i < 200000; i++){
+    LinkedList_add_last_slow(L_slow, i);
+    
+  }
+  t2 = tac();
+  printf("Performance add_last_slow: %f\n", comptime(t1, t2));
+
+  LinkedList *L_fast = LinkedList_create();
+
+  t1 = tic();
+  for (int i = 0; i < 200000; i++){
+    LinkedList_add_last(L_fast, i);
+    
+  }
+  t2 = tac();
+  printf("Performance add_last_fast: %f\n", comptime(t1, t2));
+  
+  return 0;
+ }
---
+++ apps/test_linked_list_exercises_2.c
@@ -0,0 +1,24 @@
+#include "linked_list.h"
+#include <stdio.h>
+
+int main() {
+    LinkedList *L = LinkedList_create();
+
+    //printf("First: %d\n", LinkedList__firts_val(L));
+    //printf("Last: %d\n", LinkedList_last_val(L));
+
+    LinkedList_add_last(L, 10);
+    LinkedList_add_last(L, 2);
+    LinkedList_add_last(L, 4);
+    LinkedList_add_last(L, 5);
+    LinkedList_add_last(L, 7);
+    LinkedList_print(L);
+
+    printf("First: %d\n", LinkedList_first_val(L));
+    printf("Last: %d\n", LinkedList_last_val(L));
+    printf("Get [0]: %d\n", LinkedList_get_val(L, 0)) ;                        
+
+    LinkedList_destroy(&L);
+
+    return 0;
+}
